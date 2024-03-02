390346f760899c3af819b56dc1483a228f34f5f1
Maggie-dev93 <139060168+Maggie-dev93@users.noreply.github.com>
2024-03-02 18:19:58 +0900

    Update scripts.js 1.6

--- js/scripts.js
+++ js/scripts.js
@@ -18,50 +18,41 @@ let pokemonRepository = (function() {
     }
   ];
 
-  // Define the height threshold for special Pokémon
-  let specialHeight = 10; // You can pick your own value
-
-  // Return an object with public functions
-  return {
-    getAll: function() {
-      return pokemonList;
-    },
-    add: function(pokemon) {
-      // Check if the argument is an object and has required properties
-      if (typeof pokemon === 'object' && pokemon.hasOwnProperty('name') && pokemon.hasOwnProperty('height') && pokemon.hasOwnProperty('types')) {
-        pokemonList.push(pokemon);
-      } else {
-        console.error('Invalid Pokemon object. Expected { name: "Name", height: <number>, types: ["Type1", "Type2", ...] }');
-      }
+   function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
+  }
+  function getAll() {
+    return repository;
+  }
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-// Iterate over each Pokémon in the repository using forEach() loop
-pokemonRepository.getAll().forEach(function(pokemon) {
-  // Use document.write to display the Pokémon name on the website's DOM
-  document.write(pokemon.name + " (height: " + pokemon.height + ")");
-  
-  // Check if the Pokémon's height is above the specialHeight threshold
-  if (pokemon.height > pokemonRepository.specialHeight) {
-    document.write(" - Wow, that's big!");
-  }
-  
-  // Add a line break for the next Pokémon
-  document.write("<br>");
-  
-  // Use console.log to log the output to the console
-  console.log(pokemon.name + " (height: " + pokemon.height + ")");
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
-
-
-
-
-
-
-
-  
-  
-  
-
-  
